73073228362671a3b51f7317586c78a54f870c09
Tom Bluett <tbluett@me.com>
2014-12-14 11:25:18 -0500

    Add checkbox to form

--- js/questly.js
+++ js/questly.js
@@ -1,22 +1,31 @@
-var myForm, nameField, questField, colorField, userData;
-  myForm = document.querySelector('form');
-  nameField = myForm.elements.name;
-  questField = myForm.elements.quest;
-  colorField = myForm.elements.color;
+function questly() {
+  var myForm, nameField, questField, colorField, userData;
+    myForm = document.querySelector('form');
+    nameField = myForm.elements.name;
+    questField = myForm.elements.quest;
+    colorField = myForm.elements.color;
 
-function getValues(ev) {
-  ev.preventDefault();
-  userData = {
-    name: nameField.value,
-    quest: questField.value,
-    color: colorField.value,
+  function getValues(ev) {
+    ev.preventDefault();
+    userData = {
+      name: nameField.value,
+      quest: questField.value,
+      color: colorField.value,
+      friend: this.elements.friend.checked
+    }
+    writeData ();
   }
-  writeData ();
-}
 
-function writeData() {
-  var storyDiv = document.getElementById('story');
-  storyDiv.innerHTML = userData.name + ' is on a quest to ' + userData.quest + ' with a ring that is ' + userData.color;
-}
+  function writeData() {
+    var storyDiv = document.getElementById('story');
+    var friendStory = '';
+    if (userData.friend) {
+        friendStory = 'Hey, I see you brought a friend!';
+    }
+    storyDiv.innerHTML = userData.name + ' is on a quest to ' + userData.quest +
+    ' with a ring that is ' + userData.color + friendStory;
+  }
 
-myForm.onsubmit = getValues;
+  myForm.onsubmit = getValues;
+}
+questly();
