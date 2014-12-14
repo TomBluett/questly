4cb06f9726398add57cd22bd7bb8518ff5534aa8
Tom Bluett <tbluett@me.com>
2014-12-13 20:44:11 -0500

    Write our first functions

--- js/questly.js
+++ js/questly.js
@@ -1,31 +1,20 @@
-var name = prompt('What is your name?');
+var myForm, nameField, questField, userData;
+  myForm = document.querySelector('form');
+  nameField = myForm.elements.name;
+  questField = myForm.elements.quest;
 
-if (name && (name !== 'null')) {
-  document.writeln('<p>Hi there, ' + name  +'!</p>');
-}
-else {
-  var name = 'Fred'
-  document.writeln('<p>Now your name is ' + name + '.</p>');
-}
-
-var quest = prompt('What is your quest?');
-
-if (quest && (quest !== 'null')) {
-  document.writeln('<p>'+ quest + ' sounds super cool.</p>');
-}
-else {
-  var quest = 'be the class clown'
-  document.writeln("<p>You're so boring, " + name +
-  '.  Your designated quest is ' + quest + '.</p>');
+function getValues(ev) {
+  ev.preventDefault();
+  userData = {
+    name: nameField.value,
+    quest: questField.value
+  }
+  writeData ();
 }
 
-var color = prompt('What is your favorite color?');
-
-if (!color || (color === 'null')) {
-  color = 'magenta';
+function writeData() {
+  var storyDiv = document.getElementById('story');
+  storyDiv.innerHTML = userData.name + ', ' + userData.quest;
 }
-document.writeln('<p>I love ' + color + '!</p>');
 
-document.writeln('<p>There once was a warrior named ' + name +
-  ' on a mighty quest indeed: ' + quest + '. ' + 'Luckily, ' + name +
-   ' was wearing lucky socks colored ' +color + '.</p>')
+myForm.onsubmit = getValues;
