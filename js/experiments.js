9b44ecee4ebc2b252aee359d1983bcaf92892dc7
Tom Bluett <tbluett@me.com>
2014-12-13 20:45:49 -0500

    experiments

---
+++ js/experiments.js
@@ -0,0 +1,10 @@
+var myForm, myStr1, myStr2;
+
+myForm = document.querySelector('form');
+myStr1 = myForm.elements.str1;
+myStr2 = myForm.elements.str2
+
+
+function concat(str1, str2) {
+  return str1 + " " + str2;
+}
